aebbc050268e0d03bb6e87996884afd8b7a138ae
TITOS-DEV <cdbt3980@gmail.com>
2026-08-11 10:36:51 -0500

    feat(ui): componente product-card con pipe currency y manejo de imagen vacia

---
+++ frontend/src/app/components/ui/product-card/product-card.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Product } from '../../../models/product.model';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [CurrencyPipe, RouterLink],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.css',
+})
+export class ProductCard {
+  product = input.required<Product>();
+}
