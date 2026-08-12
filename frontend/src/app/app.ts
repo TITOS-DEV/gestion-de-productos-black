67afc8ee5518167af4f42963749cf1ae2c6779e2
juancarmona721 <juan.ia0721@gmail.com>
2026-08-12 07:23:57 -0500

    feat(layout): footer y sidebar base con integración de shell

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { Navbar } from './components/layout/navbar/navbar';
+import { Sidebar } from './components/layout/sidebar/sidebar';
+import { Footer } from './components/layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Sidebar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App { }
--- frontend/src/app/components/layout/sidebar/sidebar.ts
+++ frontend/src/app/components/layout/sidebar/sidebar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
-export class Sidebar {}
+export class Sidebar { }
